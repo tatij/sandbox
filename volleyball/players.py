d03ac3d031257a80d461b84cddcd796f965285c4
tatij <kolosok.tatiana@gmail.com>
2014-10-22 11:11:57 +0300

    edit players tabs

--- volleyball/players.py
+++ volleyball/players.py
@@ -1,10 +1,10 @@
 class Player:
     def __init__(self, name, height, weight, age):
-	    self.name = name
+        self.name = name
 		self.height = height
 		self.weight = weight
 		self.age = age
-	def move_right(self):
+    def move_right(self):
 	    pass
 	def move_left(self):
 	    pass
@@ -19,13 +19,13 @@ class WeeksideHitter(Hitter):
     pass
 class MiddleBlocker(Hitter):
     def block(self, position):
-		if position == 2:
-		    while not self.meet_block():
-			    self.move_right()
-			super().block()
+        if position == 2:
+            while not self.meet_block():
+                self.move_right()
+                super().block()
 		elif position == 3:
-		    super().block()
+            super().block()
 		elif position == 4:
 		    while not self.meet_block():
 			    self.move_left()
-			super().block()
+                super().block()

120862d9ef9725cd2ba79e9d9056731d17ccf7f9
tatij <kolosok.tatiana@gmail.com>
2014-10-27 16:52:16 +0300

    edit setter and hitter

--- volleyball/players.py
+++ volleyball/players.py
@@ -2,41 +2,52 @@ class Player:
     '''
     Base class for any volleyball playyer
     '''
+    
     def __init__(self, name, height, weight, age):
         self.name = name
         self.height = height
         self.weight = weight
         self.age = age
+    
     def move_right(self):
         '''
         move player one step right
         '''
         pass
+    
     def move_left(self):
         '''
         move player one step left
         '''
         pass   
+    
     def move_front(self):
         pass
+    
     def meet_ball(self):
         pass
+    
     #def move_back(self):
         #pass
+
+
 class Hitter(Player):
     '''
     Any Hitter
     '''
+    
     def block(self, position):
         '''
         block hitter 
         '''
         pass
+    
     def meet_block(self):
         '''
         check whether player have met teammate which are going to block hit
         '''
         pass
+    
     def beat(self, position):
         if position == 2:
             self.beat_week()
@@ -44,27 +55,38 @@ class Hitter(Player):
             self.beat_middle()
         elif position == 4:
             self.beat_out()
+
+
 class Setter(Player):
+    
     def give_pass(self, position):
         self.beat(position)
+
+
 class OutsideHitter(Hitter):
     '''
     Teams outside hitters are usually the primary attackers on the team. 
     These hitters attack balls that are set to the left side of the court. 
     Outside hitters may also be referred to as outside blockers.
     '''
+    
     def beat_out(self):
         while not self.meet_ball():
             self.move_front() 
+
+
 class WeeksideHitter(Hitter):
     '''
     Team`s weak side hitters hit from the right side of the court and are 
     usually not the primary attackers on the team. 
     Weakside hitters may also be referred to as weakside blockers.
     '''
+    
     def beat_week(self):
         while not self.meet_ball():
             self.move_front()
+
+
 class MiddleBlocker(Hitter):
     '''
     On defense, the Middle Blocker position is the player at the net in the 
@@ -74,6 +96,7 @@ class MiddleBlocker(Hitter):
     hit quick sets or serve as a decoy to confuse the opponent's 
     blockers if the pass is good enough.
     '''
+    
     def block(self, position):
         '''
         block hitter 
@@ -88,6 +111,7 @@ class MiddleBlocker(Hitter):
             while not self.meet_block():
                 self.move_left()
             super().block()
+    
     def beat_middle(self):
         while not self.meet_ball():
             self.move_front()

510cddd305fac163f671415bf7991fa50a3eedd8
tatij <kolosok.tatiana@gmail.com>
2014-10-27 15:56:04 +0300

    edit setter and hitter

--- volleyball/players.py
+++ volleyball/players.py
@@ -8,15 +8,35 @@ class Player:
         pass
     def move_left(self):
         pass
+    def move_front(self):
+        pass
+    def meet_ball(self):
+        pass
+    #def move_back(self):
+        #pass
 class Hitter(Player):
     def block(self):
-	    pass
+        pass
+    def meet_block(self):
+        pass
+    def beat(self, position):
+        if position == 2:
+            self.beat_week()
+        elif position == 3:
+            self.beat_middle()
+        elif position == 4:
+            self.beat_out()
 class Setter(Player):
-    pass
+    def give_pass(self, position):
+        self.beat(position)
 class OutsideHitter(Hitter):
-    pass
+    def beat_out(self):
+        while not self.meet_ball():
+            self.move_front()
 class WeeksideHitter(Hitter):
-    pass
+    def beat_week(self):
+        while not self.meet_ball():
+            self.move_front()
 class MiddleBlocker(Hitter):
     def block(self, position):
         if position == 2:
@@ -29,3 +49,6 @@ class MiddleBlocker(Hitter):
             while not self.meet_block():
                 self.move_left()
             super().block()
+    def beat_middle(self):
+        while not self.meet_ball():
+            self.move_front()

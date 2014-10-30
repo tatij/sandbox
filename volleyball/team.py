33f5bba3b32cde6187d82a8f60867e9378e32f88
tatij <kolosok.tatiana@gmail.com>
2014-10-30 14:26:59 +0300

    tests

--- volleyball/team.py
+++ volleyball/team.py
@@ -1,9 +1,21 @@
 class Team(dict):
+    
     def is_complete(self):
         pass
+    
     def initial_positions(self):
         pass
+    
+    def min_one_setter(self):
+        pass
+    
+    def min_five_hitter(self):
+        pass
+    
+    
 class Set:
     pass
+
+
 class Game:
     pass
--- volleyball/tests.py
+++ volleyball/tests.py
@@ -22,5 +22,31 @@ class TestIsComplete(unittest.TestCase):
         )
         self.assertTrue(wg_team.is_complete())
         
+    def test_min_one_setter_false(self):
+        wg_team = Team()
+        self.assertFalse(wg_team.min_one_setter())
+        
+    def test_min_one_setter_true(self):
+        wg_team = Team()
+        wg_team[1] =  Setter('sergey', 172, 96, 32)
+        wg_team[2] =  Setter('tanya', 167, 60, 25)
+        self.assertTrue(wg_team.min_one_setter())
+        
+    def test_min_five_hitter_false(self):
+        wg_team = Team()
+        self.assertFalse(wg_team.min_five_hitter())
+        
+    def test_min_five_hitter_true(self):
+        wg_team = Team()
+        wg_team[1] =  MiddleBlocker('oleg', 192, 96, 32)
+        wg_team[2] =  OutsideHitter('dmitriy', 188, 96, 32)
+        wg_team[3] =  MiddleBlocker('alex', 191, 96, 32)
+        wg_team[4] =  OutsideHitter('igor', 194, 96, 32)
+        wg_team[5] =  OutsideHitter('max', 188, 96, 32)
+        self.assertTrue(wg_team.min_five_hitter())
+
+
+#class TestInitialPosition(TestCase):
+            
 if __name__ == '__main__':
     unittest.main()
